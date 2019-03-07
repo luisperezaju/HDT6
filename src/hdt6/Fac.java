0677888012894c01de931487b9a6f7b562c00dd2
luisperezaju <31736450+luisperezaju@users.noreply.github.com>
2019-03-06 17:54:02 -0700

    Primer Commit

---
+++ src/hdt6/Fac.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hdt6;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author Luis Perez Aju
+ */
+public class Fac {
+    public Fac(){
+        
+    }
+    
+    public static Map<String, String> obtenerMap(String opcion){
+        switch (opcion){
+            case "1":
+                return new HashMap<String, String>();
+            case "2":
+                return new HashMap<String, String>();
+            case "3":
+                return new LinkedHashMap<String, String>();
+            default:
+                return null;
+        }
+    }
+}
